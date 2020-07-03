3129548fc893913b3b6911ee2106de590fa69b52
Mac-Nikak <maximusev1290@gmail.com>
2020-07-04 02:01:04 +1000

    rename

--- Java/Yakov/Circle.java
+++ Java/Yakov/Circle.java
@@ -1,7 +1,7 @@
 public class Circle {
    public static void main(String[] args){
        double x = 1, y =1;
-       PToCAndBw in = new PToCAndBw(true,Math.sqrt(2),135);
+       PolarToCartesianAndBackwards in = new PolarToCartesianAndBackwards(true,Math.sqrt(2),135);
        System.out.println(in.getCx());
        System.out.println(in.getCy());
        System.out.println(in.getPr());
