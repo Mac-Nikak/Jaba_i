0563a84e64ab181ddd474559f97382f1842dc984
Mac-Nikak <maximusev1290@gmail.com>
2020-07-08 18:40:41 +1000

    probability theory

---
+++ Java/Maxim/Arrays/LittleTest.java
@@ -0,0 +1,6 @@
+public class LittleTest {
+    public static void main(String[] args){
+        int[] a = new int[2];
+        System.out.print(a[2]);
+    }
+}
---
+++ Java/Maxim/Arrays/TwoDice.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TwoDice {
+    public static void main(String[] args){
+        long start = System.currentTimeMillis();
+        int[] data = new int[11];
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter how many iterations do you want: ");
+        long n = keyboard.nextLong();
+        System.out.println();
+        for(int i = 0; i< n; i++){
+            int x = (int) (Math.random() * 6)  + (int) (Math.random() * 6 ) + 2;
+            data[x-2] ++;
+        }
+        for(int i = 2; i<=12; i++ ){
+
+            System.out.printf("%10d: share: %.15f number: %20d %n ", i ,data[i-2]/ (double) n, data[i-2]);
+        }
+        float elapsedTimeMillis = System.currentTimeMillis()-start / 1000F;
+        System.out.println(elapsedTimeMillis);
+    }
+}
